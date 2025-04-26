f95b6e156fd1fd8844be5afb2c74d82355b6388e
Aditya030an <adityaanu20023@gmail.com>
2025-04-26 19:55:17 +0530

    fixed missing image

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -63,7 +63,7 @@
 import React, { useState } from "react";
 import { FaPhoneAlt, FaEnvelope, FaBars, FaTimes, FaChevronDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import logo from "./photos/digitallogo.png";
+import logo from "./photos/DigitalLogo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
